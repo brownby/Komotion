8d5fb661bfaf15829aaec8a3be744ce284e4c671
jevansmith <66101462+jesmith-all@users.noreply.github.com>
2021-10-01 05:05:03 -0400

    improve getstarted.py

--- src/analysis/getStarted.py
+++ src/analysis/getStarted.py
@@ -1,11 +1,26 @@
+"""
+   Simple plotter
+   for AP10 sensing platform
+
+   Parses data from CSV file,
+   interpolates samples, applies smoothing, and plots
+
+   written for python 3.8
+   modified 1 October 2021
+   by J. Evan Smith
+
+   Active Learning Labs, Electrical Engineering
+   https://www.seas.harvard.edu/active-learning-labs
+ """
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.signal import savgol_filter
 
 plt.style.use('dark_background')
-dimensions = 9  # number of axes
-window_size, poly_order = 41, 3
+dimensions = 6  # number of columns in data less one (time)
+window_size, poly_order = 11, 3 # window must be odd
 
 with open('data_005.txt') as f:
     lines = f.readlines()
@@ -13,7 +28,7 @@ with open('data_005.txt') as f:
     for indx, line in enumerate(lines):
         tmp = np.asarray(line.replace(",\n", "").split(','))
         for indy, num in enumerate(tmp):
-            data_raw[indx, indy] = float(num)  # TODO - must have empty final line (?)
+            data_raw[indx, indy] = float(num)  # TODO - must have empty final line
 
 xinterp = np.linspace(data_raw[:, dimensions].min(), data_raw[:, dimensions].max(), len(lines))
 data_interp = interp1d(xinterp, data_raw[:, 0:dimensions], kind='linear', axis=0)
@@ -25,6 +40,6 @@ for indy, column in enumerate(data_interp.y.T):
 plt.plot(xinterp, data_smooth)
 plt.xlabel('time (s)')
 plt.ylabel('magnitude')
-plt.legend(['a_x', 'a_y', 'a_z', 'g_x', 'g_y', 'g_z', 'm_x', 'm_y', 'm_z'], loc='upper right')
-plt.title("9-axis inertial measurement: walking")
+plt.legend(['a_x', 'a_y', 'a_z', 'g_x', 'g_y', 'g_z'], loc='upper right')
+plt.title("6-axis inertial measurement: walking")
 plt.show()
