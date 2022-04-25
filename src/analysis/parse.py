9718deae254c858622a1e6314e3b095d2db24da9
jevansmith <66101462+jesmith-all@users.noreply.github.com>
2022-04-25 14:08:07 -0400

    add support for rotation (untested)

--- src/analysis/parse.py
+++ src/analysis/parse.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-fileName = 'data_a250_g100_m100_r100_v2.csv'
+fileName = 'data_a250_r100_v2.csv'
+includeRotate = 1
 
 # sweep parameters
 sr_start = 10
@@ -13,16 +14,20 @@ N = len(list(range(sr_start, sr_stop, sr_step)))  # number of runs
 
 # import data from file
 
-data = np.loadtxt(fileName, delimiter=',', usecols=(1, 2, 3, 4))
+if includeRotate:
+    data = np.genfromtxt(fileName, delimiter=',', filling_values=-1, usecols=np.arange(1, 6))
+else:
+    data = np.loadtxt(fileName, delimiter=',', usecols=np.arange(1, 5))
+
 dimen = np.loadtxt(fileName, dtype=str, delimiter=',', usecols=0)
 dimen_str = ['a', 'g', 'm', 'r']
 
 # find indices (rows) for 9-axis dimensions + rotation
 
-a_index = np.nonzero(np.char.count(dimen, 'a'))
-g_index = np.nonzero(np.char.count(dimen, 'g'))
-m_index = np.nonzero(np.char.count(dimen, 'm'))
-r_index = np.nonzero(np.char.count(dimen, 'r'))
+a_index = np.nonzero(np.char.count(dimen, dimen_str[0]))
+g_index = np.nonzero(np.char.count(dimen, dimen_str[1]))
+m_index = np.nonzero(np.char.count(dimen, dimen_str[2]))
+r_index = np.nonzero(np.char.count(dimen, dimen_str[3]))
 
 # extract dimensions + rotation into separate arrays
 # with columns x, y, z, t or r, i, j, k, t
@@ -88,6 +93,20 @@ for i, t in enumerate(m[:, 3]):
         i0 = 0
         flag = 0
 
+i0 = 0
+run = 0
+flag = 1
+for i, t in enumerate(r[:, 4]):
+    if t < 0.1:
+        i0 = i
+        flag = 1
+    if 5 - t < 0.1 and flag:
+        regression = linregress(r[i0:i, 4], list(range(i0, i)))
+        r_sr[run] = regression.slope
+        run += 1
+        i0 = 0
+        flag = 0
+
 lower_bound = 0.9 * np.array(list(range(sr_start, sr_stop, sr_step)))
 upper_bound = 2.1 * np.array(list(range(sr_start, sr_stop, sr_step)))
 
@@ -96,13 +115,13 @@ plt.style.use('dark_background')
 plt.plot(list(range(sr_start, sr_stop, sr_step)), a_sr, 'r')
 plt.plot(list(range(sr_start, sr_stop, sr_step)), g_sr, 'b')
 plt.plot(list(range(sr_start, sr_stop, sr_step)), m_sr, 'm')
-#plt.plot(list(range(10, 510, 10)), r_sr, 'c')
+plt.plot(list(range(sr_start, sr_stop, sr_step)), r_sr, 'c')
 plt.plot(list(range(sr_start, sr_stop, sr_step)), lower_bound, 'w-')
 plt.plot(list(range(sr_start, sr_stop, sr_step)), upper_bound, 'w-')
 
 plt.xlabel("set sample rate (Hz)")
 plt.ylabel("actual sample rate (Hz)")
-plt.legend(['a', 'g', 'm'])
+plt.legend(['a', 'g', 'm', 'r'])
 plt.grid(which='both', color='w', linestyle=':', linewidth=0.5)
 plt.show()
 
