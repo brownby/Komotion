41295567ec1d97fc9fb74b91cd7521f884ab48f3
jevansmith <66101462+jesmith-all@users.noreply.github.com>
2022-05-13 12:51:45 -0400

    random motion sketch

---
+++ dev/test/es20r_komotion/es20r_komotion.ino
@@ -0,0 +1,42 @@
+/*
+  Data logger for ES 20r: Physics of Sports
+
+  modified 13 May 2022
+  by J. Evan Smith
+
+  Active Learning Labs, Electrical Engineering
+  https://www.seas.harvard.edu/active-learning-labs
+*/
+
+#include <Servo.h>
+
+#define panPin 6
+#define tiltPin 7
+#define switchPin 5
+
+Servo pan;
+Servo tilt;
+
+void setup() {
+  pinMode(switchPin,INPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  pan.attach(panPin);
+  tilt.attach(tiltPin);
+
+  pan.writeMicroseconds(1500);
+  tilt.writeMicroseconds(1500);
+}
+
+void loop() {
+  if (digitalRead(switchPin)){
+    digitalWrite(LED_BUILTIN,HIGH);
+    int newPan = random(1100,1900);
+    int newTilt = random(1100,1900);
+    pan.writeMicroseconds(newPan);
+    tilt.writeMicroseconds(newTilt);
+    delay(200);
+  }
+  else{
+    digitalWrite(LED_BUILTIN,LOW);
+  }
+}
